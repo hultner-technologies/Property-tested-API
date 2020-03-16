162a90c5bf2879b9ac52f3b275e0c94ac44922d7
Alexander Hultnér <ahultner@gmail.com>
2020-03-16 17:29:35 +0100

    Started adding some samples, using schemathesis for documentation

---
+++ user-api/src/user_api/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    from pprint import pprint
+    pprint(item)
+    return {"item_name": item.name, "item_id": item_id}
